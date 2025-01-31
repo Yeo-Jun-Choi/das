55bb63caa213205f72f8be561f18478c01ada09d
Yeo-Jun-Choi <67362888+Yeo-Jun-Choi@users.noreply.github.com>
2025-01-31 14:17:30 +0900

    Delete bash directory

--- bash/run_amazon.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python main.py \
-                                    --negative_num 256 \
-                                    --dataset AmazonBooks_m1 \
-                                    --gamma 1e-7 \
-                                    --weight 2.5 \
-                                    --learning_rate 1e-3 \
-                                    --margin1 0.5 \
-                                    --margin2 0.07 \
-                                    --embedding_dim 512
--- bash/run_ml1m.sh
+++
@@ -1,9 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python main.py \
-                                    --negative_num 32 \
-                                    --dataset Movielens1M_m1 \
-                                    --gamma 1e-7 \
-                                    --weight 1.0 \
-                                    --learning_rate 1e-3 \
-                                    --margin1 1.3 \
-                                    --margin2 0.11 \
-                                    --embedding_dim 256
